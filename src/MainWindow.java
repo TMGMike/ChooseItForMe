304d9b1b61636b208b59f72575fe880b33d1e989
tmgmike <u1764905@hud.ac.uk>
2018-02-27 15:15:45 +0000

    Added 2nd panel.

--- src/MainWindow.java
+++ src/MainWindow.java
@@ -1,8 +1,7 @@
 import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.MouseAdapter;
-import java.awt.event.MouseEvent;
+import java.awt.event.*;
 import java.awt.image.BufferedImage;
 import java.beans.PropertyChangeEvent;
 import java.beans.PropertyChangeListener;
@@ -21,24 +20,123 @@ public class MainWindow {
     private JButton gamesButton;
     private JButton placesButton;
     private JButton recipesButton;
+    private JButton confirmSelectionBtn;
+    private JPanel categoryPnl;
+    private JPanel menuPnl;
+    private JComboBox comboBox1;
+    private JLabel categoryDisplayLbl;
+    private JButton backBtn;
 
     public MainWindow() {
         // ImageIcon image = new ImageIcon(new ImageIcon("tmg_logo_round256.png").getImage().getScaledInstance(64,64, Image.SCALE_DEFAULT));
-
+       // categoryPnl.setVisible(false);
+        //menuPnl.setVisible(true);
         centreLbl.setIcon(new ImageIcon(getClass().getResource("tmg_logo_round144.png")));
        // centreLbl.setMaximumSize(iconPnl.getSize());
        // centreLbl.setSize(iconPnl.getSize());
 
 
+        moviesButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                menuPnl.setVisible(false);
+                categoryPnl.setVisible(true);
+                categoryDisplayLbl.setText("Movies/TV Shows");
+                backBtn.requestFocus();
+
+            }
+        });
+        gamesButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                menuPnl.setVisible(false);
+                categoryPnl.setVisible(true);
+                categoryDisplayLbl.setText("        Games    ");
+                backBtn.requestFocus();
+            }
+        });
+        backBtn.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                menuPnl.setVisible(true);
+                categoryPnl.setVisible(false);
+            }
+        });
+        placesButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                menuPnl.setVisible(false);
+                categoryPnl.setVisible(true);
+                categoryDisplayLbl.setText("        Places    ");
+                backBtn.requestFocus();
+            }
+        });
+        recipesButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                menuPnl.setVisible(false);
+                categoryPnl.setVisible(true);
+                categoryDisplayLbl.setText("        Recipes    ");
+                backBtn.requestFocus();
+            }
+        });
+        panel1.addKeyListener(new KeyAdapter() {
+            @Override
+            public void keyPressed(KeyEvent e) {
+                super.keyPressed(e);
+
+                if(e.getKeyCode() == 27){
+                    menuPnl.setVisible(true);
+                    categoryPnl.setVisible(false);
+
+                }
+            }
+        });
+        categoryPnl.addKeyListener(new KeyAdapter() {
+            @Override
+            public void keyPressed(KeyEvent e) {
+                super.keyPressed(e);
+
+                if(e.getKeyCode() == 27){
+                    menuPnl.setVisible(true);
+                    categoryPnl.setVisible(false);
+
+                }
+            }
+        });
+        moviesButton.addKeyListener(new KeyAdapter() {
+            @Override
+            public void keyPressed(KeyEvent e) {
+                super.keyPressed(e);
+
+                if(e.getKeyCode() == 27){
+                    menuPnl.setVisible(true);
+                    categoryPnl.setVisible(false);
+
+                }
+            }
+        });
+        backBtn.addKeyListener(new KeyAdapter() {
+            @Override
+            public void keyPressed(KeyEvent e) {
+                super.keyPressed(e);
+
+                if(e.getKeyCode() == 27){
+                    menuPnl.setVisible(true);
+                    categoryPnl.setVisible(false);
+
+                }
+            }
+        });
     }
 
     public static void main(String[] args) {
-        JFrame frame = new JFrame("MainWindow");
+        final String VERSION = "0.3.5";
+        JFrame frame = new JFrame("Choose It For Me [" + VERSION + "]");
         frame.setContentPane(new MainWindow().panel1);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.pack();
         frame.setVisible(true);
         frame.setMinimumSize(new Dimension(547,534));
-
     }
 }
